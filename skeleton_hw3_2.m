8124be95a342f841485dd3f3d28866b9bc294e56
pdeveau1 <pdeveau@bu.edu>
2022-02-22 13:32:02 -0500

    initial commit

---
+++ skeleton_hw3_2.m
@@ -0,0 +1,130 @@
+% EC 414 - HW 3 - Spring 2022
+% K-Means starter code
+
+clear, clc, close all;
+
+%% Generate Gaussian data:
+% Add code below:
+mu1 = [2,2]';
+mu2 = [-2,2]';
+mu3 = [0, -3.25]';
+
+I2 = [1 0; 0 1];
+
+sigma1 = 0.02 * I2;
+sigma2 = 0.05 * I2;
+sigma3 = 0.07 * I2;
+
+points = 50;
+
+data1 =  mu1' + randn(points,2)*sigma1;
+data2 =  mu2' + randn(points,2)*sigma2;
+data3 =  mu3' + randn(points,2)*sigma3;
+% data1 = mvnrnd(mu1, sigma1, points);
+% data2 = mvnrnd(mu2, sigma2, points);
+% data3 = mvnrnd(mu3, sigma3, points);
+
+figure()
+scatter(data1(:,1), data1(:,2), 'r');
+hold on
+scatter(data2(:,1), data2(:,2), 'g');
+scatter(data3(:,1), data3(:,2), 'b');
+title('Gaussian data points')
+legend('1','2','3');
+hold off
+% %% Generate NBA data:
+% % Add code below:
+% 
+% % HINT: readmatrix might be useful here
+% 
+% % Problem 3.2(f): Generate Concentric Rings Dataset using
+% % sample_circle.m provided to you in the HW 3 folder on Blackboard.
+% 
+%% K-Means implementation
+% Add code below
+
+K = 3;
+%MU_init = [3 3; -4 -1; 2 -4];
+MU_init = [-0.14 2.61; 3.15 -0.84; -3.28 -1.58];
+
+MU_previous = MU_init;
+MU_current = MU_init;
+
+% initializations
+DATA = [data1 ; data2; data3];
+labels = ones(length(DATA),1);
+converged = 0;
+iteration = 0;
+convergence_threshold = 0.025;
+
+%stop if the derived cluster means become stationary
+while (converged==0)
+    iteration = iteration + 1;
+    fprintf('Iteration: %d\n',iteration)
+
+    %% CODE - Assignment Step - Assign each data observation to the cluster with the nearest mean:
+    % Write code below here:
+    for(i = 1:length(DATA))
+        dist = euclidean_distance(DATA(i,:),MU_current);
+        sort_dist = sort(dist);
+        closest_mu = MU_current(find(sort_dist(1,:) == dist), :);
+        label = find(closest_mu == MU_current);
+        labels(i) = label(1);
+    end
+    %% CODE - Mean Updating - Update the cluster means
+    % Write code below here:
+    MU_previous = MU_current
+    label1 = DATA(find(labels == 1),:);
+    MU_current(1,:) = mean(label1);
+    label2 = DATA(find(labels == 2),:);
+    MU_current(2,:) = mean(label2);
+    label3 = DATA(find(labels == 3),:);
+    MU_current(3,:) = mean(label3)
+
+    %% CODE 4 - Check for convergence 
+    % Write code below here:
+    
+    if (sum(sum(abs(MU_previous - MU_current) < convergence_threshold)) || sum(sum(isnan(MU_current))))
+        converged=1;
+    end
+    
+    %% CODE 5 - Plot clustering results if converged:
+    % Write code below here:
+    if (converged == 1)
+        fprintf('\nConverged.\n')
+        
+        
+       
+        
+        %% If converged, get WCSS metric
+        % Add code below
+        
+    end
+    
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+end
+
+data1 = DATA(find(labels == 1), :);
+data2 = DATA(find(labels == 2), :);
+data3 = DATA(find(labels == 3), :);
+
+figure()
+scatter(data1(:,1), data1(:,2), 'r');
+hold on
+scatter(data2(:,1), data2(:,2), 'g');
+scatter(data3(:,1), data3(:,2), 'b');
+scatter(MU_current(:,1),MU_current(:,2),'black*')
+title('Clusters produced by k-means')
+legend('1','2','3');
+hold off
+% 
+% 
+% 
+function distance = euclidean_distance(point, points)
+    distance = zeros(length(points),1);
+    [r,c] = size(points);
+    for (i = 1:r) % loop through each point of the training data
+        %distance between two points
+        distance(i,1) = sqrt((point(1,1)-points(i,1))^2+(point(1,2)-points(i,2))^2);
+    end
+end
