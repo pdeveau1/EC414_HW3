d516f4d3993186cc145101746caaeb1e5d30c4c6
pdeveau1 <pdeveau@bu.edu>
2022-02-22 21:56:17 -0500

    up to 3.2d completed

--- skeleton_hw3_2.m
+++ skeleton_hw3_2.m
@@ -17,26 +17,29 @@ sigma3 = 0.07 * I2;
 
 points = 50;
 
-data1 =  mu1' + randn(points,2)*sigma1;
-data2 =  mu2' + randn(points,2)*sigma2;
-data3 =  mu3' + randn(points,2)*sigma3;
-% data1 = mvnrnd(mu1, sigma1, points);
-% data2 = mvnrnd(mu2, sigma2, points);
-% data3 = mvnrnd(mu3, sigma3, points);
+% data1 =  mu1' + randn(points,2)*sigma1;
+% data2 =  mu2' + randn(points,2)*sigma2;
+% data3 =  mu3' + randn(points,2)*sigma3;
+data1 = mvnrnd(mu1, sigma1, points);
+data2 = mvnrnd(mu2, sigma2, points);
+data3 = mvnrnd(mu3, sigma3, points);
 
+mu = [mu1'; mu2'; mu3'];
 figure()
 scatter(data1(:,1), data1(:,2), 'r');
 hold on
 scatter(data2(:,1), data2(:,2), 'g');
 scatter(data3(:,1), data3(:,2), 'b');
+scatter(mu(:,1),mu(:,2),'black*'); %plot mu values
 title('Gaussian data points')
 legend('1','2','3');
 hold off
-% %% Generate NBA data:
-% % Add code below:
-% 
-% % HINT: readmatrix might be useful here
-% 
+
+%% Generate NBA data:
+% Add code below:
+
+% HINT: readmatrix might be useful here
+
 % % Problem 3.2(f): Generate Concentric Rings Dataset using
 % % sample_circle.m provided to you in the HW 3 folder on Blackboard.
 % 
@@ -44,82 +47,67 @@ hold off
 % Add code below
 
 K = 3;
-MU_init = [3 3; -4 -1; 2 -4];
-%MU_init = [-0.14 2.61; 3.15 -0.84; -3.28 -1.58];
-
-MU_previous = MU_init;
-MU_current = MU_init;
-
-% initializations
 DATA = [data1 ; data2; data3];
-labels = ones(length(DATA),1);
-converged = 0;
-iteration = 0;
-convergence_threshold = 0.025;
 
-%stop if the derived cluster means become stationary
-while (converged==0)
-    iteration = iteration + 1;
-    fprintf('Iteration: %d\n',iteration)
+%%(a)
+MU_init_a = [3 3; -4 -1; 2 -4];
+[MU_current_a, labels_a, WCSS_a] = k_means(K, MU_init_a, DATA);
+graph_clusters(DATA, labels_a, MU_current_a);
 
-    %% CODE - Assignment Step - Assign each data observation to the cluster with the nearest mean:
-    % Write code below here:
-    for(i = 1:length(DATA))
-        dist = euclidean_distance(DATA(i,:),MU_current); %find euclidean distance of each data point to each mu
-        labels(i) = closest_label(dist, MU_current); %find the label of the closest mu       
+%%(b)
+MU_init_b = [-0.14 2.61; 3.15 -0.84; -3.28 -1.58];
+[MU_current_b, labels_b, WCSS_b] = k_means(K, MU_init_b, DATA);
+graph_clusters(DATA, labels_b, MU_current_b);
+
+%%(c)
+%We suggest choosing centers either 1) uniformly at random from the dataset or alternatively 2) uniformly distributed over a tight rectangular region encompassing the dataset.
+size = length(DATA);
+WCSS_c = zeros(10,1);
+for(i = 1:10)
+    index = randi([1,size],1,3); %generate index for three random points from data set for mu
+    while(length(unique(index)) ~= 3) %if indices are not all unique keep generating indices
+        index = randi([1,size],1,3);
     end
-    %% CODE - Mean Updating - Update the cluster means
-    % Write code below here:
-    MU_previous = MU_current;
-    
-    for(i = 1:K) %loop through each label
-        label = DATA(find(labels == i),:); %find all data points with that label 
-        MU_current(i,:) = mean(label); %find mean of data points with label and update current mu
-    end
-    %% CODE 4 - Check for convergence 
-    % Write code below here:
-    
-    %check if the change between the previous mu and the current mu is smaller than the threshhold, if it is k-means has converged
-    %check if no points map to one of mu, if so k-means has converged
-    if (sum(sum(abs(MU_previous - MU_current) < convergence_threshold)) || sum(sum(isnan(MU_current))))
-        converged=1;
-    end  
-    %% CODE 5 - Plot clustering results if converged:
-    % Write code below here:
-    if (converged == 1)
-        fprintf('\nConverged.\n')
-        
-        
-       
-        
-        %% If converged, get WCSS metric
-        % Add code below
-        WCSS = 0;
-        for(i = 1:K) %loop through each label
-            dist = euclidean_distance(MU_current(i,:), DATA(find(labels == i),:)); %find all data points with that label
-            WCSS = WCSS + sum(dist.^2);
-        end
-    end
-    
-    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    c(i).MU_init = DATA(index, :); %get initial mu values from dataset using randomly generated indices
+    [c(i).MU_current, c(i).labels, c(i).WCSS] = k_means(K, c(i).MU_init, DATA);
+    %graph_clusters(DATA, c(i).labels, c(i).MU_current);
+    WCSS_c(i) = c(i).WCSS;
 end
+sort_WCSS = sort(WCSS_c); %sort WCSS from smallest value to largest value
+i = find(sort_WCSS(1) == WCSS_c); %find the index of the smallest WCSS
+i = i(1); %if multiple mu values generate the smallest WCSS, choose the first one
+graph_clusters(DATA, c(i).labels, c(i).MU_current); %graph clusters of smallest WCSS
 
-data1 = DATA(find(labels == 1), :);
-data2 = DATA(find(labels == 2), :);
-data3 = DATA(find(labels == 3), :);
 
+%%(d)
+size = length(DATA);
+WCSS_d = zeros(9,1);
+for(K = 2:10)
+    for(i = 1:10)
+        index = randi([1,size],1,K); %generate index for three random points from data set for mu
+        while(length(unique(index)) ~= K) %if indices are not all unique keep generating indices
+            index = randi([1,size],1,K);
+        end
+        k(K-1).d(i).MU_init = DATA(index, :); %get initial mu values from dataset using randomly generated indices
+        [k(K-1).d(i).MU_current, k(K-1).d(i).labels, k(K-1).d(i).WCSS] = k_means(K, k(K-1).d(i).MU_init, DATA);
+        %graph_clusters(DATA, d(i).labels, d(i).MU_current);
+        k(K-1).WCSS(i) = k(K-1).d(i).WCSS;
+    end
+    sort_WCSS = sort(k(K-1).WCSS); %sort WCSS from smallest value to largest value
+    WCSS_d(K-1) = sort_WCSS(1);
+end
 figure()
-scatter(data1(:,1), data1(:,2), 'r');
-hold on
-scatter(data2(:,1), data2(:,2), 'g');
-scatter(data3(:,1), data3(:,2), 'b');
-scatter(MU_current(:,1),MU_current(:,2),'black*')
-title('Clusters produced by k-means')
-legend('1','2','3');
-hold off
-% 
-% 
-% 
+plot(2:10, WCSS_d)
+xlabel('k value')
+ylabel('WCSS')
+title('WCSS change with k-value')
+
+%%(e)
+
+
+
+
+
 %returns euclidean distance of a singular point to each point in a matrix
 function distance = euclidean_distance(point, points)
     distance = zeros(length(points),1); %initialize distance vector
@@ -134,4 +122,82 @@ function label = closest_label(distance, points)
     sort_dist = sort(distance); %sort the distance vector from smalllest to largest
     close_point = points(find(sort_dist(1) == distance), :); %find the index of the smallest distance in the distance vector and find point that gives that distance
     [tf, label] = ismember(close_point, points, 'rows'); %find row index of the closest mu, correlates to label
+end
+
+%creates a scatter plot of the clusters created and their mu values given the data points, label of those points, and mu values
+function graph_clusters(data, labels, mu)
+    data1 = data(find(labels == 1), :);
+    data2 = data(find(labels == 2), :);
+    data3 = data(find(labels == 3), :);
+    
+    figure()
+    scatter(data1(:,1), data1(:,2), 'r'); %plot points with label 1
+    hold on
+    scatter(data2(:,1), data2(:,2), 'g'); %plot points with label 2
+    scatter(data3(:,1), data3(:,2), 'b'); %plot points with label 3
+    scatter(mu(:,1),mu(:,2),'black*'); %plot mu values
+    title('Clusters produced by k-means')
+    legend('1','2','3');
+    hold off
+end
+
+%takes K value, initial values of mu, and the data set
+%returns the final mu values after k_means has converged
+%returns the labels of each data point
+%returns the WCSS after convergence
+function [MU_current, labels, WCSS] = k_means(K, MU_init, DATA)
+    MU_previous = MU_init;
+    MU_current = MU_init;
+    
+    % initializations
+    
+    labels = ones(length(DATA),1);
+    converged = 0;
+    iteration = 0;
+    convergence_threshold = 0.025;
+    
+    %stop if the derived cluster means become stationary
+    while (converged==0)
+        iteration = iteration + 1;
+        fprintf('Iteration: %d\n',iteration)
+    
+        %% CODE - Assignment Step - Assign each data observation to the cluster with the nearest mean:
+        % Write code below here:
+        for(i = 1:length(DATA))
+            dist = euclidean_distance(DATA(i,:),MU_current); %find euclidean distance of each data point to each mu
+            labels(i) = closest_label(dist, MU_current); %find the label of the closest mu       
+        end
+        %% CODE - Mean Updating - Update the cluster means
+        % Write code below here:
+        MU_previous = MU_current;
+        
+        for(i = 1:K) %loop through each label
+            label = DATA(find(labels == i),:); %find all data points with that label 
+            if(~isempty(label)) %check if cluster is not empty
+                MU_current(i,:) = mean(label); %find mean of data points with label and update current mu
+            end
+        end
+        %% CODE 4 - Check for convergence 
+        % Write code below here:
+        
+        %check if the change between the previous mu and the current mu is smaller than the threshhold, if it is k-means has converged
+        if (abs(MU_previous - MU_current) < convergence_threshold)
+            converged=1;
+        end  
+        %% CODE 5 - Plot clustering results if converged:
+        % Write code below here:
+        if (converged == 1)
+            fprintf('\nConverged.\n')
+          
+            %% If converged, get WCSS metric
+            % Add code below
+            WCSS = 0;
+            for(i = 1:K) %loop through each label
+                dist = euclidean_distance(MU_current(i,:), DATA(find(labels == i),:)); %find all data points with that label
+                WCSS = WCSS + sum(dist.^2);
+            end
+        end
+        
+        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    end
 end
